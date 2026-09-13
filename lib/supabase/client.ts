7250324cc7a97fa373e46cc2bd456abbcff902b0
The Conclusion Daily <theconclusiondaily@gmail.com>
2026-09-13 16:22:51 +0530

    update 8

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -3,10 +3,5 @@ import { createBrowserClient } from "@supabase/ssr";
 export const supabase =
   createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      auth: {
-        flowType: "pkce",
-      },
-    }
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
